c779e00fb40d864e6ce6592fced44cc6cb328b43
Victor Zverovich <victor.zverovich@gmail.com>
2020-12-13 09:18:22 -0800

    Add a check

--- test.c
+++ test.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  printf("Привет, κόσμος!\n");
+  const char* message = "Привет, κόσμος!\n";
+  if ((unsigned char)message[0] != 0xD0 && (unsigned char)message[1] != 0x9F)
+    abort();
+  printf(message);
 }
